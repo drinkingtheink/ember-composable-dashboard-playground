866272972307a8940d4132fb172c64d7ad3b4dcd
Jason Harrison <jharrison@jharrison-mbp.austin.q2technology.com>
2020-01-15 12:58:38 -0600

    Link text included in content-map.

--- app/fixtures/content-map.js
+++ app/fixtures/content-map.js
@@ -2,6 +2,7 @@ const content = [
 	{
 		title: 'Neque porro quisquam',
 		link: '/bill-pay',
+		linkText: 'Pay Your Bills',
 		invertColors: true,
 		removePadding: false,
 		body: 'Nam quis metus porttitor, euismod diam sit amet, molestie lorem. Suspendisse finibus porta neque non finibus. Donec eros felis, malesuada ut posuere nec, faucibus vel lacus. Suspendisse commodo scelerisque odio elementum cursus. Cras in turpis quis mauris iaculis commodo.'
@@ -9,6 +10,7 @@ const content = [
 	{
 		title: 'Consectetur, adipisci velit',
 		link: '/accounts',
+		linkText: 'View Your Accounts',
 		invertColors: false,
 		removePadding: false,
 		body: 'Proin sagittis urna nec vestibulum consequat. Nullam eget tortor finibus, consectetur nisi sed, dictum libero. Nunc condimentum posuere quam. Duis eros nunc, malesuada at metus eget, gravida dapibus massa. Donec quis urna eros. Duis non arcu porttitor, volutpat dui a, lobortis ipsum.'
@@ -16,6 +18,7 @@ const content = [
 	{
 		title: 'Morbi ligula turpis',
 		link: '',
+		linkText: '',
 		invertColors: false,
 		removePadding: false,
 		body: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi.'
@@ -23,6 +26,7 @@ const content = [
 	{
 		title: 'Curabitur libero magna',
 		link: '/ach',
+		linkText: 'Make a Payment',
 		invertColors: true,
 		removePadding: false,
 		body: 'Maecenas elementum metus at porta tincidunt. Cras sem arcu, interdum eget ex a, auctor porta turpis. Curabitur ac dolor ex.'
@@ -30,6 +34,7 @@ const content = [
 	{
 		title: 'Consectetur, adipisci velit.',
 		link: '/goals',
+		linkText: 'Review Your Goals',
 		invertColors: false,
 		removePadding: false,
 		body: 'Cras dignissim magna eget erat tincidunt, id dapibus tellus dapibus. Donec condimentum at justo facilisis iaculis.'
