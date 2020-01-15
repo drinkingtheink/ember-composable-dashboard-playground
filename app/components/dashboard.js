64cc5efc6523cbfcd3739dca9a2a349b540ec414
Jason Harrison <jharrison@jharrison-mbp.austin.q2technology.com>
2020-01-15 12:51:14 -0600

    Initial project commit with base-outlet scaffolded.

---
+++ app/components/dashboard.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import content from '../fixtures/content-map';
+
+export default Component.extend({
+	classNames: ['app-dashboard'],
+	content: content
+});
---
+++ app/fixtures/content-map.js
@@ -0,0 +1,39 @@
+const content = [
+	{
+		title: 'Neque porro quisquam',
+		link: '/bill-pay',
+		invertColors: true,
+		removePadding: false,
+		body: 'Nam quis metus porttitor, euismod diam sit amet, molestie lorem. Suspendisse finibus porta neque non finibus. Donec eros felis, malesuada ut posuere nec, faucibus vel lacus. Suspendisse commodo scelerisque odio elementum cursus. Cras in turpis quis mauris iaculis commodo.'
+	},
+	{
+		title: 'Consectetur, adipisci velit',
+		link: '/accounts',
+		invertColors: false,
+		removePadding: false,
+		body: 'Proin sagittis urna nec vestibulum consequat. Nullam eget tortor finibus, consectetur nisi sed, dictum libero. Nunc condimentum posuere quam. Duis eros nunc, malesuada at metus eget, gravida dapibus massa. Donec quis urna eros. Duis non arcu porttitor, volutpat dui a, lobortis ipsum.'
+	},
+	{
+		title: 'Morbi ligula turpis',
+		link: '',
+		invertColors: false,
+		removePadding: false,
+		body: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi.'
+	},
+	{
+		title: 'Curabitur libero magna',
+		link: '/ach',
+		invertColors: true,
+		removePadding: false,
+		body: 'Maecenas elementum metus at porta tincidunt. Cras sem arcu, interdum eget ex a, auctor porta turpis. Curabitur ac dolor ex.'
+	},
+	{
+		title: 'Consectetur, adipisci velit.',
+		link: '/goals',
+		invertColors: false,
+		removePadding: false,
+		body: 'Cras dignissim magna eget erat tincidunt, id dapibus tellus dapibus. Donec condimentum at justo facilisis iaculis.'
+	}
+];
+
+export default content;
