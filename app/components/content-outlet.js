b990606a68538bf931cd08825400ad4dfae294cb
Jason Harrison <jharrison@jharrison-mbp.austin.q2technology.com>
2020-01-16 10:55:46 -0600

    Content outlets better determining their own dimensions.

--- app/components/content-outlet.js
+++ app/components/content-outlet.js
@@ -4,4 +4,20 @@ import { computed } from '@ember/object';
 export default Component.extend({
 	classNames: ['content-outlet'],
 	classNameBindings: ['invertColors:inverted'],
+  height: null,
+  width: null,
+  outletId: computed('outletIndex', function() {
+    return `content-outlet-${this.outletIndex}`;
+  }),
+
+  didInsertElement: function() {
+    this.setDimensions();
+  },
+
+  setDimensions() {
+    let elemHeight = document.getElementById(`${this.outletId}`).offsetHeight;
+    let elemWidth = document.getElementById(`${this.outletId}`).offsetWidth;
+    this.set(`height`, elemHeight);
+    this.set(`width`, elemWidth);    
+  },
 });
--- app/fixtures/content-map.js
+++ app/fixtures/content-map.js
@@ -37,8 +37,32 @@ const content = [
 		linkText: 'Review Your Goals',
 		invertColors: false,
 		removePadding: false,
-		body: 'Cras dignissim magna eget erat tincidunt, id dapibus tellus dapibus. Donec condimentum at justo facilisis iaculis.'
-	}
+		body: 'Cras dignissim magna eget erat tincidunt, id dapibus tellus dapibus. Donec condimentum at justo facilisis iaculis. Cras dignissim magna eget erat tincidunt, id dapibus tellus dapibus. Donec condimentum at justo facilisis iaculis. Cras dignissim magna eget erat tincidunt, id dapibus tellus dapibus. Donec condimentum at justo facilisis iaculis. Cras dignissim magna eget erat tincidunt, id dapibus tellus dapibus. Donec condimentum at justo facilisis iaculis. Cras dignissim magna eget erat tincidunt, id dapibus tellus dapibus. Donec condimentum at justo facilisis iaculis.'
+	},
+  {
+    title: 'Consectetur, adipisci velit',
+    link: '/accounts',
+    linkText: 'View Your Accounts',
+    invertColors: false,
+    removePadding: false,
+    body: 'Proin sagittis urna nec vestibulum consequat. Nullam eget tortor finibus, consectetur nisi sed, dictum libero. Nunc condimentum posuere quam. Duis eros nunc, malesuada at metus eget, gravida dapibus massa. Donec quis urna eros. Duis non arcu porttitor, volutpat dui a, lobortis ipsum.'
+  },
+  {
+    title: 'Morbi ligula turpis',
+    link: '',
+    linkText: '',
+    invertColors: false,
+    removePadding: false,
+    body: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi.'
+  },
+  {
+    title: 'Curabitur libero magna',
+    link: '/ach',
+    linkText: 'Make a Payment',
+    invertColors: true,
+    removePadding: false,
+    body: 'Maecenas elementum metus at porta tincidunt. Cras sem arcu, interdum eget ex a, auctor porta turpis. Curabitur ac dolor ex.'
+  }
 ];
 
 export default content;
